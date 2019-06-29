5bc73f864cfeeb8897c4d102aef3cfa7044267eb
Iskren Slavov <iskren.s@gmail.com>
2019-06-29 18:07:43 +0300

    Update demo dependencies

--- demo-web/bundle.js
+++ demo-web/bundle.js
@@ -1,63 +1,63 @@
-(function (reduxRemember,React,reactRedux,reactDom) {
+(function (React, reactDom, reactRedux, reduxRemember) {
     'use strict';
 
-    reduxRemember = reduxRemember && reduxRemember.hasOwnProperty('default') ? reduxRemember['default'] : reduxRemember;
     React = React && React.hasOwnProperty('default') ? React['default'] : React;
+    reduxRemember = reduxRemember && reduxRemember.hasOwnProperty('default') ? reduxRemember['default'] : reduxRemember;
 
     var SET_TEXT1 = 'SET_TEXT1';
     var SET_TEXT2 = 'SET_TEXT2';
-
     var setText1 = function setText1(text) {
-        return {
-            type: SET_TEXT1,
-            payload: text
-        };
+      return {
+        type: SET_TEXT1,
+        payload: text
+      };
     };
-
     var setText2 = function setText2(text) {
-        return {
-            type: SET_TEXT2,
-            payload: text
-        };
+      return {
+        type: SET_TEXT2,
+        payload: text
+      };
     };
 
     var text = function text() {
-        var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
-        var _ref = arguments[1];
-        var type = _ref.type,
-            payload = _ref.payload;
+      var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
 
-        switch (type) {
-            case SET_TEXT1:
-                return payload;
+      var _ref = arguments.length > 1 ? arguments[1] : undefined,
+          type = _ref.type,
+          payload = _ref.payload;
 
-            default:
-                return state;
-        }
+      switch (type) {
+        case SET_TEXT1:
+          return payload;
+
+        default:
+          return state;
+      }
     };
 
     var textToBeForgotten = function textToBeForgotten() {
-        var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
-        var _ref2 = arguments[1];
-        var type = _ref2.type,
-            payload = _ref2.payload;
+      var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
 
-        switch (type) {
-            case SET_TEXT2:
-                return payload;
+      var _ref2 = arguments.length > 1 ? arguments[1] : undefined,
+          type = _ref2.type,
+          payload = _ref2.payload;
 
-            default:
-                return state;
-        }
+      switch (type) {
+        case SET_TEXT2:
+          return payload;
+
+        default:
+          return state;
+      }
     };
 
     var reducers = {
-        persistable: {
-            text: text
-        },
-        forgettable: {
-            textToBeForgotten: textToBeForgotten
-        }
+      persistable: {
+        text: text
+      },
+      forgettable: {
+        textToBeForgotten: textToBeForgotten
+      }
     };
 
     var _reduxRemember = reduxRemember(window.localStorage),
@@ -66,191 +66,142 @@
 
     var store = createStore(combineReducers(reducers.persistable, reducers.forgettable));
 
-    var _jsxFileName = '/Users/wish/Desktop/redux-remember/demo-web/src/app.js',
-        _this = undefined;
+    var _jsxFileName = "/Users/wish/Desktop/redux-remember/demo-web/src/app.js";
 
     var App = function App(_ref) {
-        var _ref$text = _ref.text,
-            text = _ref$text === undefined ? '' : _ref$text,
-            _ref$textToBeForgotte = _ref.textToBeForgotten,
-            textToBeForgotten = _ref$textToBeForgotte === undefined ? '' : _ref$textToBeForgotte,
-            setText1$$1 = _ref.setText1,
-            setText2$$1 = _ref.setText2;
-        return React.createElement(
-            'div',
-            {
-                __source: {
-                    fileName: _jsxFileName,
-                    lineNumber: 11
-                },
-                __self: _this
-            },
-            React.createElement(
-                'h1',
-                {
-                    __source: {
-                        fileName: _jsxFileName,
-                        lineNumber: 12
-                    },
-                    __self: _this
-                },
-                'redux-remember demo (uses window.localStorage)'
-            ),
-            React.createElement(
-                'h2',
-                {
-                    __source: {
-                        fileName: _jsxFileName,
-                        lineNumber: 13
-                    },
-                    __self: _this
-                },
-                'Type something into the inputs and reload the page'
-            ),
-            React.createElement(
-                'div',
-                {
-                    __source: {
-                        fileName: _jsxFileName,
-                        lineNumber: 15
-                    },
-                    __self: _this
-                },
-                React.createElement(
-                    'label',
-                    {
-                        __source: {
-                            fileName: _jsxFileName,
-                            lineNumber: 16
-                        },
-                        __self: _this
-                    },
-                    'I shall be remembered :)'
-                )
-            ),
-            React.createElement(
-                'div',
-                {
-                    __source: {
-                        fileName: _jsxFileName,
-                        lineNumber: 19
-                    },
-                    __self: _this
-                },
-                React.createElement('input', {
-                    type: 'text',
-                    value: text,
-                    onChange: function onChange(ev) {
-                        return setText1$$1(ev.target.value);
-                    },
-                    __source: {
-                        fileName: _jsxFileName,
-                        lineNumber: 20
-                    },
-                    __self: _this
-                })
-            ),
-            React.createElement(
-                'div',
-                {
-                    __source: {
-                        fileName: _jsxFileName,
-                        lineNumber: 27
-                    },
-                    __self: _this
-                },
-                React.createElement(
-                    'label',
-                    {
-                        __source: {
-                            fileName: _jsxFileName,
-                            lineNumber: 28
-                        },
-                        __self: _this
-                    },
-                    'I shall be forgotten :('
-                )
-            ),
-            React.createElement(
-                'div',
-                {
-                    __source: {
-                        fileName: _jsxFileName,
-                        lineNumber: 31
-                    },
-                    __self: _this
-                },
-                React.createElement('input', {
-                    type: 'text',
-                    value: textToBeForgotten,
-                    onChange: function onChange(ev) {
-                        return setText2$$1(ev.target.value);
-                    },
-                    __source: {
-                        fileName: _jsxFileName,
-                        lineNumber: 32
-                    },
-                    __self: _this
-                })
-            ),
-            React.createElement(
-                'div',
-                { className: 'source-code', __source: {
-                        fileName: _jsxFileName,
-                        lineNumber: 39
-                    },
-                    __self: _this
-                },
-                React.createElement(
-                    'a',
-                    {
-                        href: 'https://github.com/zewish/redux-remember/tree/master/demo-web/src',
-                        target: '_blank',
-                        rel: 'noopener noreferrer nofollow',
-                        __source: {
-                            fileName: _jsxFileName,
-                            lineNumber: 40
-                        },
-                        __self: _this
-                    },
-                    '[ See demo source ]'
-                )
-            )
-        );
+      var _ref$text = _ref.text,
+          text = _ref$text === void 0 ? '' : _ref$text,
+          _ref$textToBeForgotte = _ref.textToBeForgotten,
+          textToBeForgotten = _ref$textToBeForgotte === void 0 ? '' : _ref$textToBeForgotte,
+          setText1 = _ref.setText1,
+          setText2 = _ref.setText2;
+      return React.createElement("div", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 11
+        },
+        __self: this
+      }, React.createElement("h1", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 12
+        },
+        __self: this
+      }, "redux-remember demo (uses window.localStorage)"), React.createElement("h2", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 13
+        },
+        __self: this
+      }, "Type something into the inputs and reload the page"), React.createElement("div", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 15
+        },
+        __self: this
+      }, React.createElement("label", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 16
+        },
+        __self: this
+      }, "I shall be remembered :)")), React.createElement("div", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 19
+        },
+        __self: this
+      }, React.createElement("input", {
+        type: "text",
+        value: text,
+        onChange: function onChange(ev) {
+          return setText1(ev.target.value);
+        },
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 20
+        },
+        __self: this
+      })), React.createElement("div", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 27
+        },
+        __self: this
+      }, React.createElement("label", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 28
+        },
+        __self: this
+      }, "I shall be forgotten :(")), React.createElement("div", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 31
+        },
+        __self: this
+      }, React.createElement("input", {
+        type: "text",
+        value: textToBeForgotten,
+        onChange: function onChange(ev) {
+          return setText2(ev.target.value);
+        },
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 32
+        },
+        __self: this
+      })), React.createElement("div", {
+        className: "source-code",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 39
+        },
+        __self: this
+      }, React.createElement("a", {
+        href: "https://github.com/zewish/redux-remember/tree/master/demo-web/src",
+        target: "_blank",
+        rel: "noopener noreferrer nofollow",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 40
+        },
+        __self: this
+      }, "[ See demo source ]")));
     };
 
     var App$1 = reactRedux.connect(function (_ref2) {
-        var text = _ref2.text,
-            textToBeForgotten = _ref2.textToBeForgotten;
-        return { text: text, textToBeForgotten: textToBeForgotten };
+      var text = _ref2.text,
+          textToBeForgotten = _ref2.textToBeForgotten;
+      return {
+        text: text,
+        textToBeForgotten: textToBeForgotten
+      };
     }, function (dispatch) {
-        return {
-            setText1: function setText1$$1(text) {
-                return dispatch(setText1(text));
-            },
-            setText2: function setText2$$1(text) {
-                return dispatch(setText2(text));
-            }
-        };
+      return {
+        setText1: function setText1$1(text) {
+          return dispatch(setText1(text));
+        },
+        setText2: function setText2$1(text) {
+          return dispatch(setText2(text));
+        }
+      };
     })(App);
 
-    var _jsxFileName$1 = '/Users/wish/Desktop/redux-remember/demo-web/src/index.js';
+    var _jsxFileName$1 = "/Users/wish/Desktop/redux-remember/demo-web/src/index.js";
+    reactDom.render(React.createElement(reactRedux.Provider, {
+      store: store,
+      __source: {
+        fileName: _jsxFileName$1,
+        lineNumber: 9
+      },
+      __self: undefined
+    }, React.createElement(App$1, {
+      __source: {
+        fileName: _jsxFileName$1,
+        lineNumber: 10
+      },
+      __self: undefined
+    })), document.getElementById('root'));
 
-    reactDom.render(React.createElement(
-        reactRedux.Provider,
-        { store: store, __source: {
-                fileName: _jsxFileName$1,
-                lineNumber: 9
-            },
-            __self: undefined
-        },
-        React.createElement(App$1, {
-            __source: {
-                fileName: _jsxFileName$1,
-                lineNumber: 10
-            },
-            __self: undefined
-        })
-    ), document.getElementById('root'));
-
-}(ReduxRemember,React,ReactRedux,ReactDOM));
+}(React, ReactDOM, ReactRedux, ReduxRemember));
--- demo-web/rollup.config.js
+++ demo-web/rollup.config.js
@@ -20,6 +20,7 @@ module.exports = {
     plugins: [
         resolve(),
         babel({
+            runtimeHelpers: true,
             exclude: 'node_modules/**'
         })
     ],
