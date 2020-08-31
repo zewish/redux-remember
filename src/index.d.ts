3897fac3c8ee7cea440a4710e3a2ad7045860dc8
Iskren Slavov <iskren.s@gmail.com>
2020-08-31 13:56:07 +0300

    Improve TypeScript definitions

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,17 +2,17 @@ import { Reducer, StoreEnhancer } from 'redux';
 
 declare namespace rememberEnhancer {
     export interface SerializeFunction {
-        (data: any): any;
-    };
+        (data: any): any
+    }
 
     export interface UnserializeFunction {
-        (data: any): any;
-    };
+        (data: any): any
+    }
 
     export interface Driver {
-        getItem(key: String): any;
-        setItem(key: String, value: any): any;
-    };
+        getItem(key: String): any,
+        setItem(key: String, value: any): any
+    }
 
     export interface Options {
         prefix?: string,
@@ -20,12 +20,16 @@ declare namespace rememberEnhancer {
         unserialize?: UnserializeFunction,
         persistThrottle?: number,
         persistWholeStore?: boolean
-    };
-};
+    }
+}
 
-export function rememberReducer(reducers: Function): Reducer<S, A>;
+export function rememberReducer(reducers: Function): Reducer;
 export function rememberEnhancer(
     driver: rememberEnhancer.Driver,
     rememberedKeys: string[],
     options?: rememberEnhancer.Options
 ): StoreEnhancer;
+
+export const REMEMBER_REHYDRATED: string;
+export const REMEMBER_PERSISTED: string;
+
